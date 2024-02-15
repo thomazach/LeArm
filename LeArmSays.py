cf672cc1c3a1075510436ee3a5f89f6ab9bb6c74
unknown <thomazac@oregonstate.edu>
2024-02-14 21:04:25 -0800

    Simon says demo assignment final

--- LeArmSays.py
+++ LeArmSays.py
@@ -1,3 +1,37 @@
+'''
+Zachary Thomas    2/14/2024
+CS 399 Intro to Robot Programming
+LeArm Says!
+
+Requirements:
+hidapi
+pyttsx3 
+pyserial
+scipy
+numpy
+
+Tested on python 3.11.4, uses match/case statements won't work on python 2
+
+Hardware:
+LeArm Hiwonder
+Arduino Uno
+Sonic Sensor (HC-SR04)
+
+Setup:
+Attach the sonic sensor to the end of the arm pointing in the direction of the gripper
+Make sure both the arduino and learm are connected to computer over usb
+Use the arduino IDE or arduino cli to find the port the arduino is connected to and update the variable ARDUINO_PORT
+Place 3 sticky notes in front of the arm and about 30-45 degrees left and right of that center sticky note
+(See anglePositions and positionDescriptors arrays for adding more in various positions)
+
+Running:
+Run this py file WITH AUDIO ON/ideally connected to a speaker. 
+
+Notes:
+Arduino code is located at sonicSensor/sonicSensor.ino  (this project does not use the arduino-cli, but it or the arduino IDE will be needed to upload sketch)
+Make sure your computer is not in the way of the sticky notes!
+'''
+
 import serial
 import time
 import threading
@@ -10,12 +44,14 @@ from Kinematics.kinematics import *
 from Kinematics.matrix import *
 from Controller.LeArm import LSC_Series_Servo, LeArm
 
+ARDUINO_PORT = "COM3"
+
 def arduino():
     global cmdList
     global response
 
     # setup arduino
-    with serial.Serial("COM4", 9600, timeout=5) as port:
+    with serial.Serial(ARDUINO_PORT, 9600, timeout=5) as port:
 
         while True:
             if cmdList[0] == True:
@@ -57,6 +93,9 @@ def countDown(integer):
         integer -= 1
 
 def MoveToPosition(linkVectors, jointAxis, desiredEndPos):
+    # I dont use this function, the new inverse kinematics are not 
+    # joint angle limited, and tackling that problem would take too 
+    # much time for this simple demo.
 
     jointAngles = []
     for servo in servos:
@@ -85,8 +124,8 @@ def main():
     print("Arduino setup complete. Ready for commands.")
 
 
-    cartesianPositions = [0, 1, 2, 3]
-    positionDescriptors = ["red sticky note.", "pink sticky note.", "blue sticky note.", "green sticky note."]
+    anglePositions = [[120.35, 92.88, 151.73, 90.9, 90], [57.78, 92.88, 151.73, 90.9, 90], [165.6, 92.88, 151.73, 90.9, 90]]
+    positionDescriptors = ["pink sticky note.", "blue sticky note.", "orange sticky note."]
     lastCommandedPlayerPosition = None
 
     # Main game loop, LeArm is simon
@@ -114,14 +153,14 @@ def main():
                     msg = msg + "The robot overlords want you to "
         
         actionStatement = randrange(2)
-        location = randrange(len(cartesianPositions) - 1)
+        location = randrange(len(anglePositions))
 
         match actionStatement:
             case 0:
                 msg = msg + "go to " + positionDescriptors[location]
 
                 if LeArmSays:
-                    lastCommandedPlayerPosition = cartesianPositions[location]
+                    lastCommandedPlayerPosition = anglePositions[location]
 
             case 1:
                 if lastCommandedPlayerPosition != None:
@@ -130,17 +169,20 @@ def main():
                     msg = msg + "spin in a circle one time."
         
         say(msg)
-        countDown(3)
+        #countDown(3)
+        say("3, 2, 1")
         thresholdSonar = 60
 
         ### Determine where player should or shouldn't be because of the commands and check that they did the correct thing
         match actionStatement:
             case 0:
 
-                MoveToPosition(linkVectors, jointAxis, cartesianPositions[location])
+                arm.servoMove(servos, anglePositions[location], time=1500)
+                time.sleep(1.5)
                 requestSonarReading()
 
                 if LeArmSays:
+                    lastCommandedPlayerPosition = anglePositions[location]
                     if int(response) > thresholdSonar:
                         gaming = False
                         break
@@ -154,8 +196,12 @@ def main():
 
 
                 if LeArmSays:
+                    if lastCommandedPlayerPosition == None:
+                        continue
+
                     ## Move arm to the previously command player position
-                    MoveToPosition(linkVectors, jointAxis, lastCommandedPlayerPosition)
+                    arm.servoMove(servos, lastCommandedPlayerPosition, time=1500)
+                    time.sleep(1.5)
                     requestSonarReading()
                     lastCommandedPlayerPosition = None
                     if int(response) < thresholdSonar: 
@@ -167,7 +213,8 @@ def main():
                         continue # Don't know where player should be
 
                     ## Move arm to previously commanded player position
-                    MoveToPosition(linkVectors, jointAxis, lastCommandedPlayerPosition)
+                    arm.servoMove(servos, lastCommandedPlayerPosition, time=1500)
+                    time.sleep(1.5)
                     requestSonarReading()
                     if int(response) > thresholdSonar:
                         print(response)
@@ -199,3 +246,4 @@ if __name__ == "__main__":
     linkVectors, jointAxis, jointAngles = buildArm()
 
     main()
+    
